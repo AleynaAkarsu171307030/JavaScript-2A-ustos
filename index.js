2e6207d8b67fbe582ebd776d3b89912d91ded969
Aleyna AKARSU <43724071+AleynaAkarsu171307030@users.noreply.github.com>
2022-08-02 22:30:38 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,12 @@
+//alert('Hello World!')
+
+// tek satırlık yorum satırları 
+
+/* 
+
+birden fazla satırı youm satırına almak istiyorsak 
+*/
+/*document.write("<h1>Merhaba JavaScript</h1>")
+
+
+document.write("<img src='download.jpeg' />")*/
